2d8adb860d01bacc7720f3fd5409678e7473bb40
ronch99 <47461841+ronch99@users.noreply.github.com>
2019-09-01 16:37:27 -0400

    Update twitterStreamingFile.py

--- learntweepy/twitterStreamingFile.py
+++ learntweepy/twitterStreamingFile.py
@@ -9,10 +9,10 @@ except ImportError:
 import tweepy
 
 # User credentials
-ACCESS_TOKEN = "822036044962902018-u61Vncw4ylohHr9WDe6fQFXT0d67hVa"
-ACCESS_SECRET = "SZiSJlrw9493tIM3rud3WHBrMfHdIutX0fwESVdXsb0Ji"
-CONSUMER_KEY = "Jr7FMGc2kTprNW5zy7sDcOh6v"
-CONSUMER_SECRET = "IAd80cz1CsQykOgCU7pdzuKVMvN3P2VuWxp6nAW9Tk3OOG9hPO"
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
 
 # Setup tweepy
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -25,3 +25,4 @@ output = open("stream083119.txt", "a")
 for status in tweepy.Cursor(api.home_timeline).items(100):
 	output.write(json.dumps(status._json)) #encode JSON
 	output.write("\n")
+output.close()

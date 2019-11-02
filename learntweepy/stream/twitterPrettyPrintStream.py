53a34740627106498e7d1fba9345430bb2593076
ronch99 <47461841+ronch99@users.noreply.github.com>
2019-11-02 16:58:12 -0400

    Update twitterPrettyPrintStream.py

--- learntweepy/stream/twitterPrettyPrintStream.py
+++ learntweepy/stream/twitterPrettyPrintStream.py
@@ -1,11 +1,6 @@
 # Reference: socialmedia-class.org/twittertutorial.html
 # Reference: developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
 # @author Ziru "Ron" Chen
-
-try:
-	import json
-except ImportError:
-	import simplejson as json
 	
 import tweepy
 
